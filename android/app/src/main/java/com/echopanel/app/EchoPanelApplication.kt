6ea905def92d22037b99e381722a6ea177a23b7a
Madhuri Sharma <madhuri.sharma921@gmail.com>
2026-09-05 08:35:36 +0530

    crash fixed

--- android/app/src/main/java/com/echopanel/app/EchoPanelApplication.kt
+++ android/app/src/main/java/com/echopanel/app/EchoPanelApplication.kt
@@ -4,4 +4,28 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class EchoPanelApplication : Application()
+class EchoPanelApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        installHoverExitCrashGuard()
+    }
+
+
+    private fun installHoverExitCrashGuard() {
+        val previousHandler = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            val isHoverExitGlitch = throwable is IllegalStateException &&
+                    throwable.message?.contains("ACTION_HOVER_EXIT") == true
+            if (isHoverExitGlitch) {
+                android.util.Log.w(
+                    "EchoPanelApplication",
+                    "Suppressed known Compose hover-exit framework glitch",
+                    throwable,
+                )
+            } else {
+                previousHandler?.uncaughtException(thread, throwable)
+            }
+        }
+    }
+}
